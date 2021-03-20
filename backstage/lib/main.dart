3232f719c942fb3dc1d15dabec97362dcc9f6fb4
Brac24 <2brac4@gmail.com>
2021-03-19 20:54:30 -0700

    creation of audience room and navigating from audience page to audience room

--- backstage/lib/main.dart
+++ backstage/lib/main.dart
@@ -202,6 +202,72 @@ class _TextButton extends StatelessWidget {
         child: TextButton(
           child: Text('Join via QR code'),
           onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AudienceRoom(),
+                ));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class AudienceRoom extends StatefulWidget {
+  @override
+  _AudienceRoomState createState() => _AudienceRoomState();
+}
+
+class _AudienceRoomState extends State<AudienceRoom> {
+  //text controller for use to retrieve the current value
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Room (Audience)'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  onChanged: (text) {
+                    print("Testing text field: $text");
+                  },
+                ),
+                TextField(
+                  controller: myController,
+                ),
+                TextButton(
+                  child: Text('Send Request'),
+                  onPressed: () {
+                    print('Pressed');
+                  },
+                )
+              ],
+            )));
+  }
+}
+
+//TODO: need to connect this roomButton to roomAudience
+class RoomButton extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          child: Text('Send Request'),
+          onPressed: () {
             print('Pressed');
           },
         ),
