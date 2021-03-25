a23f75cd5ab2d7588fb3306af73ebd7003778f1e
333xAndy <jdlc.andy@gmail.com>
2021-03-25 15:49:39 -0700

    added gridview, side bar, snackBar notfication, nothing major

--- backstage/lib/main.dart
+++ backstage/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,18 +13,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Backstage Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        //brightness and colors
+        brightness: Brightness.dark,
+        primaryColor: Colors.brown[800],
+        accentColor: Colors.deepPurple[100],
+
+        //font family
+        fontFamily: 'RobotoMono',
+
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+        ),
       ),
       home: MyHomePage(title: 'Backstage'),
     );
@@ -94,7 +99,13 @@ class _HostPageState extends State<HostPage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _createRoom,
+        onPressed: () {
+          _createRoom();
+          final snackBar = SnackBar(
+            content: Text('Room added'),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
         tooltip: 'Create Room',
         child: const Icon(Icons.add),
       ),
@@ -104,51 +115,216 @@ class _HostPageState extends State<HostPage> {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-  final List<String> actors = <String>['Host', 'Audience Member'];
-
+class AudiencePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _AudiencePageState createState() => _AudiencePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _AudiencePageState extends State<AudiencePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Audience Page'),
       ),
-      body: ListView.builder(
-          itemCount: widget.actors.length,
-          itemBuilder: (BuildContext context, int i) {
-            return ListTile(
-              title: Text(widget.actors[i]),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HostPage()));
-              },
-            );
-          }),
+      body: Center(
+          child: ElevatedButton(
+        child: Text('Join via QR'),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AudienceRoom()),
+          );
+        },
+      )),
     );
+  }
+}
+
+class AudienceRoom extends StatefulWidget {
+  @override
+  _AudienceRoomState createState() => _AudienceRoomState();
+}
+
+class _AudienceRoomState extends State<AudienceRoom> {
+  //text controller for use to retrieve the current value
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Room (Audience)'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                isDense: true,
+                contentPadding: EdgeInsets.all(8),
+              ),
+            ),
+            TextButton(
+              child: Text('Send Request'),
+              onPressed: () {
+                print('Pressed');
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RoomButton extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+            child: Text('Send Request'),
+            onPressed: () {
+              final snackBar = SnackBar(
+                content: Text('Request Sent!'),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            }),
+      ),
+    );
+  }
+}
+
+List<StaggeredTile> _staggeredTiles = const <StaggeredTile>[
+  const StaggeredTile.count(2, 2),
+  const StaggeredTile.count(2, 1),
+  const StaggeredTile.count(1, 2),
+  const StaggeredTile.count(1, 1),
+  const StaggeredTile.count(2, 2),
+  const StaggeredTile.count(1, 2),
+  const StaggeredTile.count(1, 1),
+  const StaggeredTile.count(3, 1),
+  const StaggeredTile.count(1, 1),
+  const StaggeredTile.count(4, 1),
+];
+
+List<Widget> _tiles = const <Widget>[
+  const _MyHomePageTile(Colors.green, Icons.widgets),
+  const _MyHomePageTile(Colors.lightBlue, Icons.wifi),
+  const _MyHomePageTile(Colors.amber, Icons.panorama_wide_angle),
+  const _MyHomePageTile(Colors.brown, Icons.map),
+  const _MyHomePageTile(Colors.deepOrange, Icons.send),
+  const _MyHomePageTile(Colors.indigo, Icons.airline_seat_flat),
+  const _MyHomePageTile(Colors.red, Icons.bluetooth),
+  const _MyHomePageTile(Colors.pink, Icons.battery_alert),
+  const _MyHomePageTile(Colors.purple, Icons.desktop_windows),
+  const _MyHomePageTile(Colors.blue, Icons.radio),
+];
+
+class _MyHomePageTile extends StatelessWidget {
+  const _MyHomePageTile(this.backgroundColor, this.iconData);
+
+  final Color backgroundColor;
+  final IconData iconData;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      color: backgroundColor,
+      child: new InkWell(
+        onTap: () {},
+        child: new Center(
+          child: new Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: new Icon(
+              iconData,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  final List<String> actors = <String>[
+    'Host',
+    'Audience Member',
+    'Find shows nearby'
+  ];
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: //Center(child: Text('Work in progress')),
+          new Padding(
+              padding: const EdgeInsets.only(top: 12.0),
+              child: new StaggeredGridView.count(
+                crossAxisCount: 4,
+                staggeredTiles: _staggeredTiles,
+                children: _tiles,
+                mainAxisSpacing: 4.0,
+                crossAxisSpacing: 4.0,
+                padding: const EdgeInsets.all(4.0),
+              )),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Menu'),
+              decoration: BoxDecoration(
+                color: Colors.deepPurple[100],
+              ),
+            ),
+            Container(
+              height: double.maxFinite,
+              child: ListView.builder(
+                itemCount: actors.length,
+                itemBuilder: (BuildContext context, int i) {
+                  return ListTile(
+                    title: Text(actors[i]),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => OpenPage(page: actors[i])));
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OpenPage extends StatelessWidget {
+  final String page;
+
+  //In this constructor we are requiring passing in the string name of
+  //the page we want to navigate to
+  OpenPage({Key key, @required this.page}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return (page == 'Host') ? HostPage() : AudiencePage();
   }
 }
